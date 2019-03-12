8e5a6643a5869684a3d313a0c38dc5eebdc76e9f
ktichmann <ktichmann@gmail.com>
2019-03-12 22:27:47 +0200

    add chat socket

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
 const Chat = (props) => {
+    const auth = window.sessionStorage.getItem('token')
+    const socket = io("http://127.0.0.1:3000");
 
-    useEffect(() => {
-        console.log(value)
-      });
+    socket.on('chat message', function(msg){
+        document.getElementById('chat-box').querySelector('ol').innerHTML += `<li>${msg.username}: ${msg.message}</li>`
+    })
 
     const [value, setValue] = useState( "" );
 
@@ -14,12 +16,12 @@ const Chat = (props) => {
     }
 
     const handleChat = () => {
-
+        socket.emit('chat message', {auth, message: value})
     }
 
     return (
         <div id="chat-box">
-            <ol>
+            <ol style={{height: "400px"}}>
             </ol>
             <input onChange={handleValueChange} value={value} id="chat-input" type="text" />
             <button onClick={handleChat}>Say</button>
