c9638b70351b4ec5c9089bd3d182be08e42891b1
unknown <ktichmann@gmail.com>
2019-03-14 13:51:31 +0200

    update

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,32 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const Chat = (props) => {
-    const auth = window.sessionStorage.getItem('token')
-    const socket = io("http://127.0.0.1:3000");
 
-    socket.on('chat message', function(msg){
-        document.getElementById('chat-box').querySelector('ol').innerHTML += `<li>${msg.username}: ${msg.message}</li>`
-    })
+class Chat extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            value: ''
+        }
 
-    const [value, setValue] = useState( "" );
-
-    const handleValueChange = (e) => {
-        setValue(e.target.value);
+        this.handleValueChange = this.handleValueChange.bind(this)
+        this.handleChat = this.handleChat.bind(this)
     }
 
-    const handleChat = () => {
-        socket.emit('chat message', {auth, message: value})
+    componentDidMount(){
+        this.auth = window.sessionStorage.getItem('token');
+        this.socket = io('http://127.0.0.1:5000');
+
+        this.socket.on('chat message', function(msg){
+            document.getElementById('chat-box').querySelector('ul').innerHTML += `<li>${msg.username}: ${msg.message}</li>`
+        })
+
     }
 
-    return (
-        <div id="chat-box">
-            <ol style={{height: "400px"}}>
-            </ol>
-            <input onChange={handleValueChange} value={value} id="chat-input" type="text" />
-            <button onClick={handleChat}>Say</button>
-        </div>
-    )
+    handleValueChange(e){
+        this.setState({value: e.target.value})
+    }
+
+    handleChat(){
+        const auth = this.auth;
+        this.socket.emit('chat message', {auth: auth, message: this.state.value})
+    }
+
+    render(){
+        return(
+            <div id="chat-box">
+                <ul style={{height: "400px"}}>
+                </ul>
+                <input onChange={this.handleValueChange} value={this.state.value} id="chat-input" type="text" />
+                <button onClick={this.handleChat}>Say</button>
+            </div>
+        )
+    }
 }
 
 export default Chat;
