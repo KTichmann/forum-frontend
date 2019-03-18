d076cda713e5224206f0247637bdbc3279d7873a
unknown <ktichmann@gmail.com>
2019-03-18 16:01:29 +0200

    add chat bar

---
+++ src/components/chatBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './chat';
+import './chatBar.css'
+
+const ChatBar = () => {
+    return (
+        <div>
+            <span id="chatIcon"></span>
+        </div>
+    )
+}
+
+export default ChatBar;
